6b78f7ed5f08455707fd9da84f6cc5e9f6755725
Colin Meerveld <colin.meerveld@gmail.com>
2014-03-04 09:13:09 +0100

    test local playback

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,31 +1,57 @@
 var mediaSource = new MediaSource();
+
 answerer.addEventListener("datachannel", function(e) {
 
 	var video = document.getElementsByTagName("video")[0];
 	video.src = window.URL.createObjectURL(mediaSource);
+	video.addEventListener('play', function(e) {
+		console.log("play from video")
+	});
+	video.addEventListener('canplay', function(e) {
+		console.log("canplay from video")
+	});
 
 	var channel = event.channel
+	var queue = [];
 
 	mediaSource.addEventListener('sourceopen', function(e) {
 		var sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vorbis,vp8"');
-		channel.onmessage = function(event) {
-			sourceBuffer.appendBuffer(new Uint8Array(event.data));
+		sourceBuffer.addEventListener('updateend', function() {
+		  /*if (queue.length) {
+			console.log("play next chunk")
+		    sourceBuffer.appendBuffer(queue.shift());
 			if (video.paused) {
 				console.log("play")
 				video.play();
 			}
+		  }*/
+			video.play()
+		}, false);
+		
+		
+		channel.onmessage = function(event) {
+			if (event.data == "done") {
+				sourceBuffer.appendBuffer(new Uint8Array(event.data));
+				return
+			}
+			//sourceBuffer.appendBuffer(new Uint8Array(event.data));
+			queue.push(new Uint8Array(event.data))
 		};
 
 		var url = "http://localhost:8000/big-buck-bunny_trailer.webm"
 		var xhr = new XMLHttpRequest();
 		xhr.open('GET', url, true);
 		xhr.responseType = 'arraybuffer';
+		xhr.addEventListener("load", function(){
+			sliceAndDice(new Uint8Array(xhr.response));
+		})
 		xhr.send();
-		xhr.onreadystatechange = function(e) {
+		/*xhr.onreadystatechange = function(e) {
 			if (this.readyState === 4 && this.status === 200) {
 				sliceAndDice(new Uint8Array(xhr.response));
 			}
-		};
+		};*/
+		
 	}, false);
 
 })
@@ -38,7 +64,7 @@ function sliceAndDice(response) {
 	})
 	var reader = new FileReader();
 	reader.onload = function(e) {
-		var data = e.target.result
+		/*var data = e.target.result
 		var dataSent = 0;
 		var charSlice = 1000;
 
@@ -52,12 +78,13 @@ function sliceAndDice(response) {
 			console.log(dataSent, file.size)
 			if (dataSent + 1 >= file.size) {
 				console.log("All data chunks sent.");
-				offererDataChannel.send("\n");
-				mediaSource.endOfStream();
+				offererDataChannel.send("done");
+				//mediaSource.endOfStream();
 				clearInterval(intervalID);
 			}
-		}, 20);
+		}, 20);*/
+		mediaSource.sourceBuffers[0].appendBuffer(new Uint8Array(e.target.result))
+		
 	}
-	//reader.readAsArrayBuffer(file);
-	reader.readAsDataURL(file);
+	reader.readAsArrayBuffer(file);
 };
